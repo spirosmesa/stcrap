9d945acd2c126872fc7baa5f946791242b4056fc
spyros mesa <spirosmesa@yahoo.gr>
2020-04-20 17:41:01 +0200

    adding stuff

---
+++ sources/m183_LTL_CTL.c
@@ -0,0 +1,166 @@
+#include <stdio.h> 
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+
+    extern void __VERIFIER_error(int);
+
+	// inputs
+	int inputs[] = {55,56,57,58,59};
+
+	void calculate_output(int);
+	void calculate_outputm1(int);
+	void calculate_outputm2(int);
+	void calculate_outputm3(int);
+	void calculate_outputm4(int);
+	void calculate_outputm5(int);
+	void calculate_outputm6(int);
+	void calculate_outputm7(int);
+	void calculate_outputm8(int);
+
+	 int a1504052271 = 140;
+	 int a355441369 = 296;
+	 int a553774011 = 4;
+	 int cf = 1;
+
+ void calculate_outputm1(int input) {
+    if(((input == 56) &&  cf==1 )) {
+    	cf = 0;
+    	a355441369 = ((((((((a355441369 * a1504052271) % 14999) % 95) + 212) + -3724) * 5) % 95) + 250);
+    	a1504052271 = (((((((a1504052271 * a355441369) % 14999) % 12) + 146) * 1) - -26865) - 26866); 
+    	 printf("%d\n", 64); fflush(stdout); 
+    } if(( cf==1  && (input == 57))) {
+    	cf = 0;
+    	a1504052271 = (((((((a1504052271 * a355441369) % 14999) % 105) + 27) + -4968) - -11739) - 6772);
+    	a553774011 = 7; 
+    	 printf("%d\n", 60); fflush(stdout); 
+    } 
+}
+ void calculate_outputm2(int input) {
+    if(( cf==1  && (input == 57))) {
+    	cf = 0;
+    	a553774011 = 9; 
+    	 printf("%d\n", 60); fflush(stdout); 
+    } if(((input == 56) &&  cf==1 )) {
+    	cf = 0;
+    	a355441369 = (((((a1504052271 * a1504052271) * 1) % 48) + 68) * 1);
+    	a1504052271 = ((((a1504052271 * a355441369) - -12704) - -528) + 896); 
+    	 printf("%d\n", 64); fflush(stdout); 
+    } 
+}
+ void calculate_outputm3(int input) {
+    if(((input == 59) &&  cf==1 )) {
+    	cf = 0;
+    	a1504052271 = (((((a1504052271 % 12) + 146) * 1) - 18935) + 18934);
+    	a355441369 = (((((a1504052271 * a1504052271) / 5) % 95) + 164) - 33); 
+    	 printf("%d\n", 60); fflush(stdout); 
+    } 
+}
+ void calculate_outputm4(int input) {
+    if(( cf==1  && (input == 56))) {
+    	cf = 0;
+    	a355441369 = ((((a1504052271 * a1504052271) - -10765) + 1445) - -189);
+    	a1504052271 = (((((((a1504052271 * a355441369) % 14999) - 8929) / 5) * 5) % 12) + 145); 
+    	 printf("%d\n", 63); fflush(stdout); 
+    } 
+}
+ void calculate_outputm5(int input) {
+    if(( cf==1  && (input == 56))) {
+    	cf = 0;
+    	a1504052271 = ((((a1504052271 + 9836) * 10) / 9) / 5);
+    	a355441369 = (((((((a1504052271 * a1504052271) % 14999) % 48) + 50) * 9) / 10) + -18); 
+    	 printf("%d\n", 66); fflush(stdout); 
+    } 
+}
+ void calculate_outputm6(int input) {
+    if(( cf==1  && (input == 57))) {
+    	cf = 0;
+    	a1504052271 = (((((((a1504052271 * a355441369) % 14999) % 105) - 14) / 5) - 24176) - -24226);
+    	a553774011 = 4; 
+    	 printf("%d\n", 63); fflush(stdout); 
+    } if(((input == 58) &&  cf==1 )) {
+    	cf = 0;
+    	 
+    	 printf("%d\n", 64); fflush(stdout); 
+    } if(((input == 55) &&  cf==1 )) {
+    	cf = 0;
+    	a1504052271 = (((((((a1504052271 * a355441369) % 14999) % 105) + 4) / 5) - -5260) - 5306);
+    	a553774011 = 2; 
+    	 printf("%d\n", 62); fflush(stdout); 
+    } 
+}
+ void calculate_outputm7(int input) {
+    if(( cf==1  && (input == 59))) {
+    	cf = 0;
+    	a355441369 = (((((((a355441369 * a1504052271) % 14999) % 95) + 212) * 5) % 95) - -169); 
+    	 printf("%d\n", 64); fflush(stdout); 
+    } 
+}
+ void calculate_outputm8(int input) {
+    if(( cf==1  && (input == 57))) {
+    	cf = 0;
+    	 
+    	 printf("%d\n", 65); fflush(stdout); 
+    } if(( cf==1  && (input == 58))) {
+    	cf = 0;
+    	a1504052271 = ((((((((a1504052271 * a355441369) % 14999) + -19076) * 10) / 9) + 29218) * -1) / 10);
+    	a355441369 = ((((((a355441369 * a1504052271) % 14999) + 17432) * 1) + -12283) - -19411); 
+    	 printf("%d\n", 61); fflush(stdout); 
+    } 
+}
+
+ void calculate_output(int input) {
+        cf = 1;
+
+    if(( cf==1  && a1504052271 <=  -80)) {
+    	if((308 < a355441369 &&  cf==1 )) {
+    		calculate_outputm1(input);
+    	} 
+    } 
+    if((((-80 < a1504052271) && (132 >= a1504052271)) &&  cf==1 )) {
+    	if((a553774011 == 2 &&  cf==1 )) {
+    		calculate_outputm2(input);
+    	} 
+    	if((a553774011 == 4 &&  cf==1 )) {
+    		calculate_outputm3(input);
+    	} 
+    	if(( cf==1  && a553774011 == 7)) {
+    		calculate_outputm4(input);
+    	} 
+    	if((a553774011 == 9 &&  cf==1 )) {
+    		calculate_outputm5(input);
+    	} 
+    } 
+    if(( cf==1  && ((132 < a1504052271) && (158 >= a1504052271)))) {
+    	if((((116 < a355441369) && (308 >= a355441369)) &&  cf==1 )) {
+    		calculate_outputm6(input);
+    	} 
+    	if((308 < a355441369 &&  cf==1 )) {
+    		calculate_outputm7(input);
+    	} 
+    } 
+    if(( cf==1  && 158 < a1504052271)) {
+    	if(( cf==1  && ((19 < a355441369) && (116 >= a355441369)))) {
+    		calculate_outputm8(input);
+    	} 
+    } 
+
+
+    if( cf==1 ) 
+    	fprintf(stderr, "Invalid input: %d\n", input); 
+}
+
+int main()
+{
+    // main i/o-loop
+    while(1)
+    {
+        // read input
+        int input;
+        scanf("%d", &input);        
+        // operate eca engine
+        if((input != 55) && (input != 56) && (input != 57) && (input != 58) && (input != 59))
+          return -2;
+        calculate_output(input);
+    }
+}
